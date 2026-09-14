69160f91bd57917a7e31088269cc41ec9187a801
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2026-09-14 22:36:05 +0700

    fix: add load env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,13 +4,27 @@ import (
 	"log"
 
 	"github.com/arlinggacr/BtechDevCases/internal/config"
+	"github.com/arlinggacr/BtechDevCases/internal/database"
 	"github.com/arlinggacr/BtechDevCases/internal/module/auth"
 	"github.com/arlinggacr/BtechDevCases/internal/server"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf(".env not loaded: %v", err)
+	}
 	cfg := config.Load()
-	authModule := auth.NewModule(cfg.JWTSecret, cfg.TokenTTL)
+	db, err := database.Connect(cfg.Database)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if err := database.InitializeSchema(db); err != nil {
+		log.Fatal(err)
+	}
+
+	authModule := auth.NewModule(db, cfg.JWTSecret, cfg.TokenTTL)
 	app := server.New(authModule)
 
 	log.Printf("server listening on %s", cfg.Address)
